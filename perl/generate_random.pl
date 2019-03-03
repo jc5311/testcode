477413691e6744d4c89695a4cae134f1a521370c
JC5311 <jc.psp5@gmail.com>
2019-03-03 00:19:23 -0500

    Made created file names to be random digits instead of characters.

--- perl/generate_random.pl
+++ perl/generate_random.pl
@@ -3,8 +3,7 @@
 #use strict; use warnings;
 use String::Random;
 
-print "This program generates a specified number of random files within\n";
-print "its working directory!\n";
+print "This program generates a specified number of random files within its working directory!\n";
 print "Are you sure you wish to continue? [y or n] "; $decision = <STDIN>;
 chomp $decision;
 
@@ -16,7 +15,7 @@ if ($decision eq "y")
     my $string_gen = String::Random->new;
     for (my $i = 0; $i < $num_to_gen; $i++)
     {
-        $file = ($string_gen->randpattern("ccc")).'.txt';
+        $file = ($string_gen->randpattern("nnnnn")).'.txt';
         open(FH, '>'.$file) or die "error";
         close $file;
     }
