81e5f9f73e8061e8249381cfedb50ab016634096
JC5311 <jc.psp5@gmail.com>
2019-03-03 00:12:54 -0500

    Created a perl app that generates random files. Needs fixing however, I don't know how to make it stop generating files.

---
+++ perl/generate_random.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+#use strict; use warnings;
+use String::Random;
+
+print "This program generates a specified number of random files within\n";
+print "its working directory!\n";
+print "Are you sure you wish to continue? [y or n] "; $decision = <STDIN>;
+chomp $decision;
+
+#Begin work
+if ($decision eq "y")
+{
+    print "How many files should be generated? "; $num_to_gen = <STDIN>;
+    chomp $num_to_gen;
+    my $string_gen = String::Random->new;
+    for (my $i = 0; i < 3; $i++)
+    {
+        $file = ($string_gen->randpattern("ccc")).'.txt';
+        open(FH, '>'.$file) or die "error";
+        close $file;
+    }
+    print "Done.\n";
+}
+
+print "Program terminating...\n";
